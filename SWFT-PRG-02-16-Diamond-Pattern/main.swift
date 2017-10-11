798ab8d311c746603ba671b312535de37bdb515e
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-11 09:00:34 -0400

    Initial Commit

---
+++ SWFT-PRG-02-16-Diamond-Pattern/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SWFT-PRG-02-16-Diamond-Pattern
+//
+//  Created by Keith Smith on 10/11/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
